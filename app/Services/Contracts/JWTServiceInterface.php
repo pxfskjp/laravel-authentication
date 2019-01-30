bcbf61e5775e36ee4ad5164580717e393e553177
topdeveloper0729 <topdev0729@gmail.com>
2019-01-30 18:39:43 +0200

    Refactoring - JWTService - method rename

--- app/Services/Contracts/JWTServiceInterface.php
+++ app/Services/Contracts/JWTServiceInterface.php
@@ -4,7 +4,11 @@
 namespace App\Services\Contracts;
 
 
+use Illuminate\Http\Response;
+
 interface JWTServiceInterface
 {
     public function validateToken(): array;
+
+    public function createTokenByCredentials(string $identity, string $password): Response;
 }
--- app/Services/JWTService.php
+++ app/Services/JWTService.php
@@ -6,6 +6,8 @@ namespace App\Services;
 
 use App\Services\Contracts\JWTServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
 
 class JWTService implements JWTServiceInterface
 {
@@ -14,5 +16,23 @@ class JWTService implements JWTServiceInterface
         return [
             'status' => 'success', 'code' => 200
         ];
+    }
+
+    public function createTokenByCredentials(string $identity, string $password): Response
+    {
+        $tokenRequest = Request::create(
+            config('app.url') . config('auth.passport.token.link'),
+            'POST',
+            [
+                'grant_type' => config('auth.passport.grant'),
+                'client_id' => config('auth.passport.client.id'),
+                'client_secret' => config('auth.passport.client.secret'),
+                'username' => $identity,
+                'password' => $password,
+                'scope' => '*'
+            ]
+        );
+        app()->instance('request', $tokenRequest);
+        return Route::dispatch($tokenRequest);
     }
 }
